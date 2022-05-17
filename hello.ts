9ca72aa47f31f10d71587a8b389c8170080cfa28
David Eduardo Karpinski <105662189+DavidKarpinski@users.noreply.github.com>
2022-05-17 15:02:50 -0300

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -1,11 +1,26 @@
-const message:string = `Hello World!`;
-
-function exibition:void(message:string) {
-  console.log(message);
-  alert(message);
-  document.write(message);
+class Error {
+  public static error:string = "Error: something is wrong!";
+  
+  public static warning() {
+    alert(error);
+    console.log(error);
+    document.write(error);
+  }
 }
 
-if (2 > 1) {
-  exibition(message);
+
+
+window.onload() = () => {
+  if (2 > 1) {
+    function exibition:void() {
+      const message:string = "Hello World!";
+      console.log(message);
+      alert(message);
+      document.write(message);
+    }
+    exibition();
+  } else {
+    var bloomer:Error = new Error();
+    bloomer.warning();
+  }
 }
