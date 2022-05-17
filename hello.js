547a8b0deb53e5b51c6aca6b285c846323f61d0d
David Eduardo Karpinski <105662189+DavidKarpinski@users.noreply.github.com>
2022-05-17 14:37:28 -0300

    Creating file hello.js!

---
+++ hello.js
@@ -0,0 +1,11 @@
+const message = "Hello World!";
+
+function exibition(message) {
+  alert(message);
+  console.log(message);
+  document.write(message);
+}
+
+if (2 > 1) {
+  exibition(message);
+}
